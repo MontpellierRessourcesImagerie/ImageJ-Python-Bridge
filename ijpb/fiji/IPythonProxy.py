281cf28a6dae518236f257facb5b2d3f0e5d8c49
volker-baecker <volker.baecker@mri.cnrs.fr>
2021-06-13 00:12:18 +0200

    renamed the env, removed example from IPythonProxy

--- ijpb/fiji/IPythonProxy.py
+++ ijpb/fiji/IPythonProxy.py
@@ -64,12 +64,3 @@ class IPythonProxy:
 		self.thread.join()
 		self.python.terminate()
 
-p = IPythonProxy()
-p.run("from ij import IJ")
-p.run("a = 3")
-p.run("print(a)", wait=True)
-p.run("a = a+3")
-p.run("print(a)", wait=True)
-p.run('print("Hello")', wait=True)
-p.run('IJ.setProperty("py_res", a)')
-p.disconnect()
