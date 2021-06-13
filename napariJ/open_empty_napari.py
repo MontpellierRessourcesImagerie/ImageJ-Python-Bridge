ee8457e15537cb6bd926dc00d40391d829a06a41
volker-baecker <volker.baecker@mri.cnrs.fr>
2021-06-13 13:21:40 +0200

    open empty viewer without astronaut

--- napariJ/open_empty_napari.py
+++ napariJ/open_empty_napari.py
@@ -1,9 +1,8 @@
 from ijpb.fiji.IPythonProxy import IPythonProxy
 
 p = IPythonProxy()
-p.run("from skimage import data")
 p.run("import napari")
 p.run("from PyQt5 import QtCore")
-p.run("viewer = napari.view_image(data.astronaut(), rgb=True)")
+p.run("viewer = napari.Viewer()")
 # p.run("QtCore.QTimer.singleShot(0, napari.view_image(data.astronaut(), rgb=True))")
 p.disconnect()
